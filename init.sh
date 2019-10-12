1807fa3344adc0ac48f03f4a5279200a08c5ac2a
zhristoph <zhristoph@tutanota.com>
2019-10-12 13:32:22 +0200

    Corrected curl usage

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ set -eEuo pipefail
 micropythonGithub="https://github.com/micropython/micropython.git"
 micropythonLibGithub="https://github.com/micropython/micropython-lib.git"
 esp32ToolchainFile="xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz"
-esp32ToolchainURL="https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz"
+esp32ToolchainURL="https://dl.espressif.com/dl/${esp32ToolchainFile}"
 
 echo "Initialising wlanProbe"
 echo "Checking for micropython repo"
@@ -17,6 +17,6 @@ if [ ! -d micropython-lib ]; then
 fi
 echo "Checking for ESP32 toolchain"
 if [ ! -f "micropythondev/${esp32ToolchainFile}" ]; then
-    /usr/bin/curl "${esp32ToolchainURL}" > "micropythondev/${esp32ToolchainFile}"
+    /usr/bin/curl "${esp32ToolchainURL}" --output "micropythondev/${esp32ToolchainFile}"
 fi
 echo "Finished intialising the project"
