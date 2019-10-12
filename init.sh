91d897eb7636dbb708a0feeb08f70a531b30b4bc
zhristoph <cmptr@arcology.lan>
2019-10-12 11:38:38 +0200

    Added script to initialise the project

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eEuo pipefail
+
+micropythonGithub="https://github.com/micropython/micropython.git"
+micropythonLibGithub="https://github.com/micropython/micropython-lib.git"
+esp32ToolchainFile="xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz"
+esp32ToolchainURL="https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz"
+
+echo "Initialising wlanProbe"
+echo "Checking for micropython repo"
+if [ ! -d micropython ]; then
+    /usr/bin/git clone "${micropythonGithub}" micropython
+fi
+echo "Checking for micropython-lib repo"
+if [ ! -d micropython-lib ]; then
+    /usr/bin/git clone "${micropythonLibGithub}" micropython-lib
+fi
+echo "Checking for ESP32 toolchain"
+if [ ! -f "micropythondev/${esp32ToolchainFile}" ]; then
+    /usr/bin/curl "${esp32ToolchainURL}" > "micropythondev/${esp32ToolchainFile}"
+fi
+echo "Finished intialising the project"
