a61af531a177bc87d3f562ecf23ea0c4d7632e83
Michael Richardson <mcr@sandelman.ca>
2019-11-15 21:28:17 -0500

    added build for out-of-tree python

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ -f .settings.sh ]
+then
+    . ./.settings.sh
+fi
+
+if [ -z "$MICROPYTHON" ] || [ -z "$TARGET" ]
+then
+    echo "Please set MICROPYTHON= directory with micropython build"
+    echo "Please set TARGET=      board type (within ports/)"
+    echo Current values are:
+    echo MICROPYTHON=$MICROPYTHON
+    echo TARGET=$TARGET
+    echo Please also set variables for micropython.
+    exit 1
+
+fi
+
+# make build directory fully-qualified
+HERE=$(pwd)
+BUILD=$HERE/build-${TARGET}
+
+mkdir -p $BUILD
+#make -C $MICROPYTHON/ports/$TARGET FROZEN_MPY_DIR=${HERE}/py FLASH_MODE=dio $*
+make -C $MICROPYTHON/ports/$TARGET BUILD=$BUILD FROZEN_MPY_DIR=${HERE}/py FLASH_MODE=dio $*
+#make -C $MICROPYTHON/ports/$TARGET BUILD=$BUILD FLASH_MODE=dio $*
+
